2a8fb1d082346ac97407d0e701db4d60a51607dd
andres <ivanandresbuve@unicauca.edu.co>
2020-10-04 10:07:28 -0500

    Primer push de prueba

---
+++ src/prueba1sistemas/Prueba1Sistemas.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba1sistemas;
+
+/**
+ *
+ * @author JUAN
+ */
+public class Prueba1Sistemas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
